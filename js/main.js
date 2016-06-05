0b25e5b63381e1050b1bd221530c026133160143
Andrey Makar-Uvarov <lyykfi@gmail.com>
2016-06-05 18:29:50 +0300

    fx

--- js/main.js
+++ js/main.js
@@ -17,7 +17,18 @@ $$(".panel-left a").on("click", function() {
 
 var toggler = document.getElementById("hesed_toggler");
 var isTogle = false;
+
 toggler.addEventListener("click", function() {
     isTogle = !isTogle;
     document.getElementById("hesed_items").classList.toggle("show", isTogle);
+});
+
+$$(document).on("click", "a.external", function(event) {
+    myApp.popup('.popup-frame');
+    event.stopPropagation();
+    event.preventDefault();
+
+    document.getElementById("frame").setAttribute("src", event.target.getAttribute("href"));
+    document.getElementById("frame").setAttribute("height", parseInt(window.innerHeight, 10) - 100);
+    return false;
 });
